7554812721880316b2bf5e928feebdea58cc6755
Alexander F. Rødseth <rodseth@gmail.com>
2023-09-02 00:51:49 +0200

    Minor changes

--- zsh-noregerts.zsh
+++ zsh-noregerts.zsh
@@ -1,19 +1,19 @@
 #!/bin/zsh
 
 command_not_found() {
-    echo 'Updating files database...'
-    sudo pacman -Fy --noconfirm >/dev/null 2>&1
-
     echo "Checking if $1 is available in the path..."
     if command -v $1 >/dev/null 2>&1; then
         echo "$1 is already installed"
         exec $@
     fi
 
+    echo 'Updating files database...'
+    sudo pacman -Fy --noconfirm >/dev/null 2>&1
+
     echo "Checking if $1 is available in the Arch Linux repositories..."
 
-    # Get a list of packages that contain the command name anywhere in the file path
-    packages=$(LC_ALL=C pacman -F $1 2>/dev/null | awk '{print $1}')
+    # Get a list of unique package names that contain the command name anywhere in the file path
+    packages=$(LC_ALL=C pacman -F $1 2>/dev/null | awk '{print $1}' | sort -u)
     if [ -z "$packages" ]; then
         echo "Command '$1' not found, and no package found for installation."
         exit 1
@@ -22,8 +22,7 @@ command_not_found() {
     # Iterate over each package and check if it contains /usr/bin/$1
     found_package=""
     for package in ${(f)packages}; do
-        echo "Checking package contents: $package"
-        if pacman -F -l $package 2>/dev/null | grep -q "usr/bin/$1"; then
+        if pacman -F -l $package 2>/dev/null | grep -q "bin/$1"; then
             found_package=$package
             break
         fi
